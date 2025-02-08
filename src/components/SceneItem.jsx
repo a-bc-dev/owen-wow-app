b9424792bc3a759b749d760a866145656fceb5f1
a-bc-dev <biancadragan11@gmail.com>
2025-02-08 20:05:25 +0100

    Remove scene item link text decoration

--- src/components/SceneItem.jsx
+++ src/components/SceneItem.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function SceneItem({ scene, id }) {
   return (
     <li className="scene__card">
-      <Link to={`/detail/${id}`} className="scene-link">
+      <Link to={`/detail/${id}`} className="scene__link">
         <article className="scene__info">
           <img className="scene__info__image" src={scene.poster} alt={`Poster of ${scene.movie}`} />
           <h3 className="scene__info__title">{scene.movie} - {scene.year}</h3>
