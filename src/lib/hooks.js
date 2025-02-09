579e06ae2f0277902b16419c71e2213dea79914f
a-bc-dev <biancadragan11@gmail.com>
2025-02-09 11:54:48 +0100

    Store scenes in localStorage

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import { generateSceneId } from "./utils";
 
 function useFetchScenes() {
-    const [scenes, setScenes] = useState([]);
-  
-    useEffect(() => {
+  const [scenes, setScenes] = useState([]);
+
+  useEffect(() => {
+    const storedScenes = localStorage.getItem("scenes");
+    if (storedScenes) {
+      setScenes(JSON.parse(storedScenes));
+    } else {
       fetch("https://owen-wilson-wow-api.onrender.com/wows/random?results=50")
         .then((response) => response.json())
         .then((data) => {
@@ -12,11 +16,13 @@ function useFetchScenes() {
             ...scene,
             generatedId: generateSceneId(scene),
           }));
+          localStorage.setItem("scenes", JSON.stringify(updatedScenes));
           setScenes(updatedScenes);
         });
-    }, []);
-  
-    return { scenes };
-  }
-  
-  export { useFetchScenes };
+    }
+  }, []);
+
+  return { scenes };
+}
+
+export { useFetchScenes };
