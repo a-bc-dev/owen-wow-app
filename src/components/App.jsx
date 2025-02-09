3e12e4e710df1e725035573f33728b34bcc572a3
a-bc-dev <biancadragan11@gmail.com>
2025-02-09 11:21:05 +0100

    Use Header component in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,15 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
-import Header from "./Header.jsx";
+import Header from "./Header.jsx"; 
 import LandingPage from "../pages/LandingPage.jsx";
 import DetailPage from "../pages/DetailPage.jsx";
 import "../styles/main.scss";
 import { useFetchScenes } from "../lib/hooks";
 
 function App() {
-  const { scenes } = useFetchScenes();  // Solo obtenemos scenes
-
+  const { scenes } = useFetchScenes();  
   return (
     <div className="page">
-      <header className="header">
-        <h1 className="header__title">Owen Wilson's WoW</h1>
-      </header>
+      <Header title="Owen Wilson's WoW" />
       <main>
         <Routes>
           <Route index element={<LandingPage scenes={scenes} />} />
